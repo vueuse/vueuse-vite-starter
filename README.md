2496594ecae7593ed18b36f2d5b78a31ee9f351e
Anthony Fu <anthonyfu117@hotmail.com>
2020-05-24 04:05:12 +0800

    chore: init

---
+++ README.md
@@ -0,0 +1,23 @@
+<h1 align='center'>⚡️ Vite VueUse Starter</h1>
+
+<p align='center'>
+<img src='./src/logo.svg' height='120'/>
+</p>
+
+
+Starter for [Vite](https://github.com/vuejs/vite) + [VueUse](https://github.com/antfu/vueuse) + [TypeScript](https://www.typescriptlang.org/)
+
+🌐 [Live Demo](https://vite-vueuse-starter.netlify.app/)
+
+📦 [Webpack Example](https://github.com/antfu/vueuse-next-example)
+
+## Installation
+
+1. Clone this repository
+2. `yarn`
+3. `yarn dev`
+4.  open http://localhost:3000 in your browser
+
+## License
+
+[MIT](http://opensource.org/licenses/MIT)
---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+    <img src="./logo.svg" />
+    <h1>Hello <a href='https://github.com/vuejs/vite' target='__blank'>Vite</a> and <a href='https://github.com/antfu/vueuse' target='__blank'>VueUse</a>!</h1>
+
+    <h3>Mouse: {{x}} x {{y}}</h3>
+    <h3>
+      Counter: {{count}}
+      <a @click='inc()' style='margin-right:10px'>+</a>
+      <a @click='dec()'>-</a>
+    </h3>
+
+    <br/><br/>
+    <p><a href='https://github.com/antfu/vite-vueuse-starter' target='__blank'>Source</a></p>
+    <p><a href='https://vueuse-next-example.netlify.app/' target='__blank'>Webpack Example</a></p>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+import { useMouse, useCounter } from '@vueuse/core'
+
+export default {
+  setup() {
+    const { x, y } = useMouse()
+    const { count, inc, dec } = useCounter()
+
+    return {
+      x, 
+      y,
+      count,
+      inc,
+      dec,
+    }
+  }
+}
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css?family=Noto+Serif&display=swap');
+
+html, body, h1, h2, h3, p {
+  font-family: 'Noto Serif', serif;
+  user-select: none;
+}
+
+#app {
+  text-align: center;
+  color: rgba(0,0,0,0.4);
+}
+img {
+  width: 500px;
+}
+a {
+  color: #41b883;
+  text-decoration: none;
+  cursor: pointer;
+}
+</style>
